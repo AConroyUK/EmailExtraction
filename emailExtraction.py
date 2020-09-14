199b7035542d796d28f7ada936ac7073137ef313
CONROY Aedan <aconroy@AltranUK.local>
2020-09-14 16:05:09 +0100

    Dictionary

--- emailExtraction.py
+++ emailExtraction.py
@@ -2,13 +2,22 @@ import re
 import sys
 
 def main(argv):
+    dict = {}
     with open("sample.txt", "r") as file:
         for line in file:
             for word in line.split():
                 if (len(argv)>0 and argv[0] == '-n'):
-                    if re.search("\w*@\w*",word) != None: print(word)
+                    if re.search("@",word) != None: print(word)
                 else:
-                    if re.search("\w*@[a-z]*(.com|.net)$|\.co\.[a-z]{2}$",word) != None: print(word)
+                    if re.search("\w+@[a-z\-]+\.[a-z]+\.?[a-z]+$",word) != None:
+                        #print(word)
+                        domain = word.split("@")
+                        if domain[1] in dict:
+                            dict[domain[1]] += 1
+                        else: dict[domain[1]] = 1
+    for key in list(dict):
+        print(key + ": " + str(dict[key]))
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
