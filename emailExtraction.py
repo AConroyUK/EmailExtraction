66e8e82f38601323ec2c224176bd9374efbcbb5f
CONROY Aedan <aconroy@AltranUK.local>
2020-09-14 14:37:33 +0100

    regular expressions

--- emailExtraction.py
+++ emailExtraction.py
@@ -1,1 +1,14 @@
+import re
+import sys
 
+def main(argv):
+    with open("sample.txt", "r") as file:
+        for line in file:
+            for word in line.split():
+                if (len(argv)>0 and argv[0] == '-n'):
+                    if re.search("\w*@\w*",word) != None: print(word)
+                else:
+                    if re.search("\w*@[a-z]*(.com|.net)$|\.co\.[a-z]{2}$",word) != None: print(word)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
